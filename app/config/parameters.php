0c975ac8ae01ba3328f3648ca1de3a279ad58ea8
Panda Serwer <m.ciepln27@gmail.com>
2025-12-19 17:43:27 +0100

    Initial PrestaShop 8 installation

---
+++ app/config/parameters.php
@@ -0,0 +1,64 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => 'sklepfizjoterapeuty.pandacoders.com.mysql.dhosting.pl',
+    'database_port' => '',
+    'database_name' => 'biec7i_sklepfiz',
+    'database_user' => 'aeyai9_sklepfiz',
+    'database_password' => 'veet1ShoiNgu',
+    'database_prefix' => 'sf_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'tHakjQOW5uBMfbxpPhE37u1fdqtKeHtxGFKn0GRarU8aPU82M7796NE89PHeGkaX',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2025-12-19',
+    'locale' => 'pl-PL',
+    'use_debug_toolbar' => true,
+    'cookie_key' => 'Zt9eL1tdDWN5JCDNHmHOWdnoimTbCGLz3q0Q2xQCWwUNMUC22SrSuKXYb0UQYWd3',
+    'cookie_iv' => '9o6vpxhRWX1A9Lwr3XxhEU2iz9NtGx20',
+    'new_cookie_key' => 'def00000c4a89babcd0d306aae73da02102fad26346deed7e32e25d347aec216b020da4354d431a5261b274ebc1dd30ee68fcf6170112b51185bb938e1f0bc0c07b805cd',
+    'api_public_key' => '-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvOJ9ovmK6rJgFHUZsx8y
+7U81/Trd++5ISKak+TNi7lULA+Dg7LIoSwK41L1zeokWO9mI0YIlhxyaIVY4q2AW
+GJSlYY03AHThJ2tlszlzVfiMbk3SO4qEkblXqy/3PB6FP4BcIMmNfn8PMXKAD+CV
+Z/ak0otZSZHbS9ImEVU7Et7i519YVcFF/YSE1ozY1xO85sSE01YWaVFrESOd0VcZ
+xFZV32Y2BbTPvFUwrHMyzcc892l8nfZtzkP/4zQXFuMvIgXCGMEUEOjHSCSfTc/g
+T1JkDOsqDTCEvoaljPrmTx4bWl8yLm7hu7BFWrZycPsp9QEQSrNBCNIBssfzt2Vy
+RQIDAQAB
+-----END PUBLIC KEY-----
+',
+    'api_private_key' => '-----BEGIN PRIVATE KEY-----
+MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC84n2i+YrqsmAU
+dRmzHzLtTzX9Ot377khIpqT5M2LuVQsD4ODssihLArjUvXN6iRY72YjRgiWHHJoh
+VjirYBYYlKVhjTcAdOEna2WzOXNV+IxuTdI7ioSRuVerL/c8HoU/gFwgyY1+fw8x
+coAP4JVn9qTSi1lJkdtL0iYRVTsS3uLnX1hVwUX9hITWjNjXE7zmxITTVhZpUWsR
+I53RVxnEVlXfZjYFtM+8VTCsczLNxzz3aXyd9m3OQ//jNBcW4y8iBcIYwRQQ6MdI
+JJ9Nz+BPUmQM6yoNMIS+hqWM+uZPHhtaXzIubuG7sEVatnJw+yn1ARBKs0EI0gGy
+x/O3ZXJFAgMBAAECggEAS70qEbqsaHXELw/RkVC2VBAKHxOGxamDlJY12rMvPVAe
+jZyTtHWv0Hwz7yJUdtMXdkJBKXldYPMoHqv7J0TtN0gLlLVSU72KNwtKiXZE+Dkk
+3moYHcXqaKCANeWS5ql6Vr86se7/7LQ4PGSkUzmDz19IMiCsWeLBo4KNvy+E84h5
+z5VQB57NfXYqb/UfozZvOmeLOMpTgYzIMTUy5c/B674tWSSliEp50eb3bvctReMw
+v5gpgmkoHpYRxQIq1Eu4et3yi2hZUmGHGQSf6PAI7KOOVklwyDyIxr41Hcs9VLiu
+V/RBab9WK/Y56sGF+s60rl2a7HqN7ZvxZmPkefju9QKBgQDj1GStZ/naLXX7Hv/8
+Y4jMniEw1gazyLHeX7uNEJmwKXYO8ehFCxpdA0O3QnHiEKmWTIKODSlGgvbW2UCS
+XfbgzmWNGSpS1RujCeRG2uy4o756LMWlKmBxEdyz6denPudc8jlpl7ap1fMIloza
+JRU+amkiLyyECUn5DD7nIZxohwKBgQDUPVqgXB075p8pu0uYovvUdpdocZIrwQzf
+XsY3l7YoWcqXYW/PNJDcfmWFjYRi9HaTeoupYoMwoQ4W8Czv56X0RPhiV5iATu9y
+Z5P33+gEcp0mHApKZAXFxJ5fSfWdibYQ7AEaT6gsuItBGfjcM7tznJLfE0y7kwVP
+mHI01Dwd0wKBgQC97jOcKGEvlNSVbCuF0ciapH0RBGwD9xzpQK/PEva82GO2Y875
+uB7bSbxrC3Z7Ot7WEKSKHci6IrX7OJRPMQx+LzKTpVYWRdjLW5S1yQ6tgrxph+eh
+IKnTuUtfpWOF5rfkUX6xW6wI+/amwzNbLK0AIp/Qwrj+veuwNpg+AVQuAwKBgFQ0
+64oIQgkMHUYfa4Tp1WSckMH9nXMCqyrMnXRdGwazaW/aVtQtFPSGqZztYwMn3wvw
+aQ0Rat+WZtKvBAVDMVLTMoFCI910dAg+FDUafyGUn8kJAwQ03x1yPyurWmngw32/
+RUi9JgpoRcfUmCo11D+wdY1QSPd7A3ZrE5TmR9vFAoGANY8WHh63IjUDknGBq9fk
+p0wXeeceaqpt9RhEXmEGyjKDGZKBy8Q8dp+fytT1babj9+Yko9YfU481zJiNhTTs
+B5buJ7W2YUMPKJepiN8V4a0VFgN598t+sRq03ngFoq2XWwPZ94XaNzvJUQ51NKHR
+3sI27XwkyAKD9QXuzOeT1ws=
+-----END PRIVATE KEY-----
+',
+  ),
+);
